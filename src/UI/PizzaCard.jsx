89592eba6d4794f31b67425a0b46508e9d535226
Tarık Çekirge <tcekirge@gmail.com>
2025-04-15 15:11:46 +0300

    image fix

--- src/UI/PizzaCard.jsx
+++ src/UI/PizzaCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PizzaCard = ({ url = "/order", name, price, rating, reviews, image }) => {
     return (
         <Link to={url} className='bg-white rounded-2xl p-8 space-y-2 hover:shadow-2xl transition-shadow'>
-            <img src={`http://localhost:5173/src/assets/images/products/${image}`} alt={name} />
+            <img src={`src/assets/images/products/${image}`} alt={name} />
             <h2 className='text-gray-950 font-barlow font-600 text-lg'>{name}</h2>
             <div className='flex items-center justify-between text-base font-barlow font-600'>
                 <span className='flex-2 text-gray-600'>{rating} </span>
